f4efb072384b16a42967d848118c8f06af3b4036
vigneshkalithas <vigneshkanna3333@gmail.com>
2022-11-30 11:13:33 +0530

    after env update

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-// app.use(cors());
-app.use(
-  cors({
-    origin: "https://beamish-pavlova-50ad58.netlify.app/login",
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "https://beamish-pavlova-50ad58.netlify.app/login",
+//   })
+// );
 
 try {
   await mongoose.connect(process.env.MONGO_URL);
